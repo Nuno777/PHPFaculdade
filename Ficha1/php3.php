e34de53b2dfa60207c8bdd070d97fe0a2cf50c27
Nuno Miguel <nm.albano20@gmail.com>
2022-04-01 11:43:25 +0100

    exercico 3 e comeco do 4

---
+++ Ficha1/php3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP: Concatenação de Strings</title>
+</head>
+<body>
+    <?php
+    $nome = "Nuno";
+    $apelido = "Miguel";
+    echo "Meu nome é ".$nome." ".$apelido."";
+    ?>
+</body>
+</html>
