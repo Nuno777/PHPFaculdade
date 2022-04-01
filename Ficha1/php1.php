e289756e9ccd995039fd724045a2b8b6a73c0e60
Nuno Miguel <nm.albano20@gmail.com>
2022-04-01 10:22:15 +0100

    inicio da introdução ao php

---
+++ Ficha1/php1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP: Utilização variáveis</title>
+</head>
+<body>
+    <?php
+    echo "Ola mundo";
+    ?>
+</body>
+</html>
