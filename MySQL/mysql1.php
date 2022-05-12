5f6f0c4b441a99a553b1d635ba3e771838261bd8
Nuno Miguel <nm.albano20@gmail.com>
2022-05-12 20:47:56 +0100

    naos sie mysql

--- MySQL/mysql1.php
+++ MySQL/mysql1.php
@@ -1,25 +1,32 @@
+<?php
+$host = "localhost";
+$database = "pws";
+$user = "root";
+$pass = "";
+$conn = new mysqli($host, $user, $pass, $database);
+session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
-<?php
-$host="localhost";
-$database="pws";
-$user="root";
-$pass="";
-$conn = new mysqli($host, $user, $pass, $database);
-?>
+
 <body>
-    <label for="">Email</label>
-    <input type="email" name="email" id="email">
-    <br>
-    <label for="">Password</label>
-    <input type="password" name="password" id="password">
-    <br>
-    <button type="submit">Login</button>
+    <form action="listuser.php" method="post">
+        <label for="">Email</label>
+        <input type="email" name="email" id="email">
+        <br>
+        <label for="">Password</label>
+        <input type="password" name="password" id="password">
+        <br>
+        <button type="submit">Login</button>
+    </form>
 </body>
+
 </html>
