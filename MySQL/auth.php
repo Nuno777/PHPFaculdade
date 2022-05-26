00ad17f5f23c5bd9684981f64ac4a42c683b5feb
Nuno Miguel <nm.albano20@gmail.com>
2022-05-26 11:02:38 +0100

    auth fix sem database

--- MySQL/auth.php
+++ MySQL/auth.php
@@ -3,22 +3,21 @@ session_start();
 $_SESSION['errors'] = array(); // Cleanup previous errors
 if (isset($_POST['email'])) $email = trim($_POST['email']);
 else $email = "";
-if (isset($_POST['pass'])) $password = trim($_POST['pass']);
+if (isset($_POST['password'])) $password = trim($_POST['password']);
 else $password = "";
 if (strlen($email) == 0)
     $_SESSION['errors']['email'] = 'Empty email';
 if (strlen($password) == 0)
-    $_SESSION['errors']['pass'] = 'Empty password';
+    $_SESSION['errors']['password'] = 'Empty password';
 if (count($_SESSION['errors']) == 0) {
     if (strcmp($email, $password) == 0) { // Some dummy authentication
         $_SESSION['authenticated'] = true;
         $_SESSION['email'] = $email;
-        header('Location: logout.php');
+        header('Location: listUser.php');
     } else
         $_SESSION['errors']['auth'] = 'Authentication failed';
 }
-if (count($_SESSION['errors']) != 0) {
+ if (count($_SESSION['errors']) != 0) {
     header('Location: login.php');
     exit(0);
 }
-?>
--- MySQL/login.php
+++ MySQL/login.php
@@ -1,22 +1,25 @@
 <?php
 session_start();
+/* if (!isset($_SESSION['authenticated'])) {
+    header('Location: private.php'); //pagina privada
+    exit(0);
+}  */
 require_once 'conecao.php';
-if(isset($_POST['login'])){
-    $email=($_POST['email']);
-    $password=($_POST['password']);
-     $password=hash('sha512',$password);//segurança
-     $sql="SELECT * FROM utilizador WHERE email='$email' AND pass='$password'";
-     $result=mysqli_query($conn,$sql);
-     if(mysqli_num_rows($result)){
-         $_SEESION['message']="Login com sucesso.";
-         $_SEESION['email']=$email;
-         $_SEESION['password']=$password;
-         header("location: listuser.php"); //vai para o calendario
-     }
-     else{
-         $_SEESION['message']="Email ou Password incorreta, tente novamente.";
-     }
- }
+if (isset($_POST['login'])) {
+    $email = ($_POST['email']);
+    $password = ($_POST['password']);
+    $password = hash('sha512', $password); //segurança
+    $sql = "SELECT * FROM utilizador WHERE email='$email' AND pass='$password'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result)) {
+        $_SEESION['message'] = "Login com sucesso.";
+        $_SEESION['email'] = $email;
+        $_SEESION['password'] = $password;
+        header("location: listuser.php"); //vai para o calendario
+    } else {
+        $_SEESION['message'] = "Email ou Password incorreta, tente novamente.";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,12 +34,12 @@ if(isset($_POST['login'])){
 </head>
 
 <body>
-    <form class="form" method="POST" action="login.php" enctype="multipart/form-data">
+    <form class="form" method="POST" action="auth.php" enctype="multipart/form-data">
         <img src="img/estg.png" alt="">
         <br><br>
         <div class="form-input">
             <label for="">Email</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="Escreva o email" required>
+            <input type="text" class="form-control" id="email" name="email" placeholder="Escreva o email" required>
         </div>
         <div class="form-input">
             <label for="">Password</label>
@@ -45,7 +48,7 @@ if(isset($_POST['login'])){
         <?php
         if (isset($_SEESION['message'])) {
             echo "<div id='error_msg'>" . $_SEESION['message'] . "</div>";
-            //unset($_SEESION['message']);
+            unset($_SEESION['message']);
         }
         ?>
         <br>
