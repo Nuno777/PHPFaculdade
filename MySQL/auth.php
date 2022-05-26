8b5c7f0bb473f965852ed5535207a7ad9455ebd4
Nuno Miguel <nm.albano20@gmail.com>
2022-05-26 11:50:51 +0100

    fazer auth com database

--- MySQL/auth.php
+++ MySQL/auth.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
-$_SESSION['errors'] = array(); // Cleanup previous errors
+require_once 'conecao.php';
+$email = ($_POST['email']);
+$password = ($_POST['password']);
+$password = hash('sha512', $password);
+$sql = "SELECT * FROM utilizador WHERE email='$email' AND pass='$password'";
+$result = mysqli_query($conn, $sql);
+$_SESSION['errors'] = array(); // Limpar erros anteriores
 if (isset($_POST['email'])) $email = trim($_POST['email']);
 else $email = "";
 if (isset($_POST['password'])) $password = trim($_POST['password']);
@@ -10,14 +16,15 @@ if (strlen($email) == 0)
 if (strlen($password) == 0)
     $_SESSION['errors']['password'] = 'Empty password';
 if (count($_SESSION['errors']) == 0) {
-    if (strcmp($email, $password) == 0) { // Some dummy authentication
+    if (strcmp($email, $password) == 0) { // Alguma autenticação fictícia
         $_SESSION['authenticated'] = true;
         $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
         header('Location: listUser.php');
     } else
         $_SESSION['errors']['auth'] = 'Authentication failed';
 }
- if (count($_SESSION['errors']) != 0) {
+if (count($_SESSION['errors']) != 0) {
     header('Location: login.php');
     exit(0);
 }
