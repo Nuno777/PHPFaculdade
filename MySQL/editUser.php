fa3ba9fb84b95e7f24d540fadca52de1480c9e66
Nuno Miguel <nm.albano20@gmail.com>
2022-06-02 11:49:09 +0100

    edit ja com value

--- MySQL/editUser.php
+++ MySQL/editUser.php
@@ -1,53 +1,3 @@
-<?php
-session_start();
-if (!isset($_SESSION['authenticated'])) {
-    header('Location: login.php');
-    exit(0);
-}
-$email = array_key_exists('email', $_GET) ? $_GET['email'] : "";
-$nome = array_key_exists('nome', $_GET) ? $_GET['nome'] : "";
-$pass = array_key_exists('pass', $_GET) ? $_GET['pass'] : "";
-// $foto = array_key_exists('foto', $_FILES) ? $_FILES['foto']['name'] : "";
-
-$msg_erro = "";
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($email == "" || $pass == "" || $nome == "")
-        $msg_erro = "Email, nome ou password não inseridos";
-    else {
-        require_once 'conecao.php';
-        if ($conn->connect_errno) {
-            $code = $conn->connect_errno;
-            $message = $conn->connect_error;
-            $msg_erro = "Falha na ligação à BaseDados ($code $message)!";
-        } else {
-            $email = $conn->real_escape_string($email);
-            $nome = $conn->real_escape_string($nome);
-            $pass_hash = hash('sha512', $pass);
-
-            $query = "UPDATE utilizador SET email='$email' ,nome='$nome' ,pass='$pass_hash' WHERE email='$email'";
-            /* if ($foto != "" && getimagesize($_FILES['foto']['tmp_name'])) {
-                // tratar upload da foto
-                $diretoria_upload = "uploads/";
-                $extensao = pathinfo($foto, PATHINFO_EXTENSION);
-                $novo_ficheiro = $diretoria_upload . sha1(microtime()) . "." . $extensao;
-
-                if (move_uploaded_file($_FILES['foto']['tmp_name'], $novo_ficheiro)) {
-                    $query = "UPDATE utilizador SET email='$email' ,nome='$nome' ,pass='$pass',foto='$novo_ficheiro' WHERE email='$email'";
-                }
-            } */
-            $sucesso_query = mysqli_query($conn, $query);
-            if ($sucesso_query) {
-                header("Location: listuser.php");
-                exit(0);
-            } else {
-                $code = $conn->errno;
-                $message = $conn->error;
-                $msg_erro = "Falha na query! ($code $message)";
-            }
-        }
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -66,33 +16,85 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         require_once 'navbar.php';
         ?>
     </header>
-    <div class="container">
-        <form action="editUser.php" id="editUser" class="form" method="POST" enctype="multipart/form-data">
-            <div class="row">
-                <div class="col-12" class="form">
-                    <h2>Edit User</h2>
-                    <br>
+
+    <?php
+    session_start();
+    if (!isset($_SESSION['authenticated'])) {
+        header('Location: login.php');
+        exit(0);
+    }
+    require_once 'conecao.php';
+    $email = $_GET["email"];
+    $query = "SELECT nome,foto FROM utilizador WHERE email='$email'";
+    $result = mysqli_query($conn, $query);
+    if ($result && $result->num_rows) {
+        $row = $result->fetch_object();
+        $nome = $row->nome;
+        $foto = $row->foto;
+        if ($foto == null) {
+            $foto = "";
+        }
+    ?>
+        <div class="container">
+            <form action="editUser.php" name="editUser" class="form" method="POST" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="col-12" class="form">
+                        <h2>Edit User</h2>
+                        <br>
+                    </div>
                 </div>
-            </div>
-            <div class="form-input">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email" value="<?= $email ?>" required>
-            </div>
-            <div class="form-input">
-                <label for="nome">Nome</label>
-                <input type="text" class="form-control" id="nome" name="nome" value="<?= $nome ?>" required>
-            </div>
-            <div class="form-input">
-                <label for="pass">Password</label>
-                <input type="password" class="form-control" id="pass" name="pass" value="<?= $pass ?>" required>
-            </div>
-            <div class="form-input">
-                <label for="idFoto">Foto</label>
-                <input type="file" class="form-control" id="foto" name="foto"><br>
-            </div>
-            <button type="submit" class="btn btn-primary" name="update">Edit</button>
-        </form>
-    </div>
+                <div class="form-input">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" value="<?= $email ?>" required>
+                </div>
+                <div class="form-input">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control" id="nome" name="nome" value="<?= $nome ?>" required>
+                </div>
+                <div class="form-input">
+                    <label for="idFoto">Foto</label>
+                    <input type="file" class="form-control" id="foto" name="foto"><br>
+                </div>
+                <button type="submit" class="btn btn-primary" name="edit">Edit</button>
+            </form>
+        </div>
+    <?php
+    } else {
+        echo "<script>alert('Selecione um utilizador valido')</script> <?php header('Location: listuser.php')?>";
+    }
+    /*     
+
+    $msg_erro = "";
+    if ($conn->connect_errno) {
+    $code = $conn->connect_errno;
+    $message = $conn->connect_error;
+    $msg_erro = "Falha na ligação à BaseDados ($code $message)!";
+    } else {
+    $email = $conn->real_escape_string($email);
+    $nome = $conn->real_escape_string($nome);
+    $query = "SELECT nome,foto FROM utilizador WHERE email='$email'";
+    $result = mysqli_query($conn, $query);
+    if ($result && $result->num_rows) {
+    $row = $result->fetch_object();
+    $nome = $row->nome;
+    $foto = $row->foto;
+    if ($foto == null) {
+    $foto = "";
+    } /* else {
+    echo "<script>
+        alert('Selecione um utilizador valido')
+    </script>";
+
+    }
+    header("Location: listuser.php");
+    } else {
+    $code = $conn->errno;
+    $message = $conn->error;
+    $msg_erro = "Falha na query! ($code $message)";
+    }
+    }  */
+    ?>
+
 </body>
 
 
